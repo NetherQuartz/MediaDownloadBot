d7d5cf0996f3ed56d4b38fbf08d0f9e277e97b48
Vladimir Larkin <vladimir@larkin.one>
2024-01-14 22:22:18 +0300

    Add inline downloading for twitter

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,17 +1,22 @@
 import os
 import asyncio
 import re
+import hashlib
+import logging
+
+import telebot
 
 from telebot.async_telebot import AsyncTeleBot
-from telebot.types import Message
+from telebot import types
 
 from .download import get_twitter_video
 
+telebot.logger.setLevel(logging.DEBUG if os.getenv("LOGGING_LEVEL") == "DEBUG" else logging.INFO)
 bot = AsyncTeleBot(token=os.getenv("TOKEN"))
 
 
 @bot.message_handler(commands=["start"])
-async def start(message: Message) -> None:
+async def start(message: types.Message) -> None:
     await bot.send_message(message.chat.id, "Привет")
     
 
@@ -19,7 +24,7 @@ twitter_pattern = r"(?:https?://)?(?:x.com|twitter.com)/.+/status/\d+"
 
 
 @bot.message_handler(regexp=twitter_pattern)
-async def download_twitter(message: Message) -> None:
+async def download_twitter(message: types.Message) -> None:
     progress_msg = await bot.reply_to(message, "🔎")
     
     url = re.findall(twitter_pattern, message.text)[0]
@@ -36,6 +41,26 @@ async def download_twitter(message: Message) -> None:
         await bot.reply_to(message, text="It seems not to be a link to video 😔")
     
     await bot.delete_message(message.chat.id, progress_msg.id)
+
+
+@bot.inline_handler(lambda query: re.match(twitter_pattern, query.query))
+async def inline_download_twitter(query: types.InlineQuery) -> None:
+    url = re.findall(twitter_pattern, query.query)[0]
+    video = await get_twitter_video(url, return_url=True)
+
+    if video:
+        await bot.answer_inline_query(
+            query.id,
+            results=[
+                 types.InlineQueryResultVideo(
+                    id=hashlib.md5(video.video_url.encode()).hexdigest(),
+                    video_url=video.video_url,
+                    mime_type="video/mp4",
+                    thumbnail_url=video.thumbnail_url,
+                    title="Click here to send"
+                 )
+            ]
+        )
 
 
 asyncio.run(bot.infinity_polling())
--- bot/download.py
+++ bot/download.py
@@ -2,11 +2,19 @@ import urllib.parse
 
 import aiohttp
 
+from dataclasses import dataclass
+
 from io import BytesIO
 from bs4 import BeautifulSoup
 
 
-async def get_twitter_video(tweet_url: str) -> BytesIO | None:
+@dataclass
+class WebVideo:
+    video_url: str
+    thumbnail_url: str
+
+
+async def get_twitter_video(tweet_url: str, return_url: bool = False) -> BytesIO | WebVideo | None:
     
     tweet_url = urllib.parse.quote(tweet_url, safe="")
     query_url = f"https://twitsave.com/info?url={tweet_url}"
@@ -14,6 +22,13 @@ async def get_twitter_video(tweet_url: str) -> BytesIO | None:
     async with aiohttp.ClientSession() as session, session.get(query_url) as response:
         text = await response.text()
         data = BeautifulSoup(text, "html.parser")
+
+        if return_url:
+            videos = data.find_all("video", class_="aspect-video")
+            if not videos:
+                return None
+            return WebVideo(video_url=videos[0].get("src"), thumbnail_url=videos[0].get("poster"))
+
         download_button_candidates = data.find_all("div", class_="origin-top-right")
         if download_button_candidates:
             download_button = download_button_candidates[0]
